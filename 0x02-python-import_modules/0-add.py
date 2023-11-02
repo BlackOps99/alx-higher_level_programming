e39dac94fcc02645f2abed55d48f9228c174ff14
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-11-02 13:47:52 +0200

    [ADD] Task 1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
 
-    from add_0 import add
+    import calculator_1 as cl
 
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, cl.add(a, b)))
+    print("{} - {} = {}".format(a, b, cl.sub(a, b)))
+    print("{} * {} = {}".format(a, b, cl.mul(a, b)))
+    print("{} / {} = {}".format(a, b, cl.div(a, b)))

1614ed6ec8c818c45055a7744de4da12348c8e8d
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-11-04 14:39:47 +0200

    [FIX] Task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     if idx < 0:
         return 'None'
 
-    lenOfList = len(my_list)
+    lenOfList = len(my_list) - 1
 
     if idx > lenOfList:
         return 'None'

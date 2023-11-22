807097cdb24d7ecd9414ff1b52e37384dbca3c3d
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2023-11-22 20:38:55 +0200

    [Add] Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Define a class named Square."""
+
+
+def __init__(self, size):
+    """Initialize a new class Square.
+
+    Args:
+        size (int): The size of the new square.
+    """
+    self.__size = size

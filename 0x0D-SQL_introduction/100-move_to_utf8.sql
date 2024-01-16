bcf1de9183667d125806e744fdcfa1ac9ef2f0e4
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2024-01-16 21:45:52 +0200

    [FIX] Task 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,8 @@
--- Convert the table's default charset and collation
--- and convert the charset and collation of the 'name' field
+-- Set the character set and collation for the database
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-USE `hbtn_0c_0` ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Set the character set and collation for the table
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Set the character set and collation for the specific field
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

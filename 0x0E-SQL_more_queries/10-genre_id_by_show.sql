30d30e6cc6113945bc4f12ba5f1b17103e4af873
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2024-01-20 19:42:06 +0200

    [FIX] Task 10 2

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,18 @@
 -- script that lists all shows contained in hbtn_0d_tvshows
-SELECT DISTINCT t.`title`, s.`genre_id`
-FROM `tv_shows` AS t
-INNER JOIN `tv_show_genres` AS s ON t.`id` = s.`show_id`
-WHERE t.`id` IN (SELECT DISTINCT `show_id`
-                 FROM `tv_show_genres`)
-ORDER BY t.`title`, s.`genre_id`;
+SELECT DISTINCT `name`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON g.`id` = s.`genre_id`
+
+       INNER JOIN `tv_shows` AS t
+       ON s.`show_id` = t.`id`
+       WHERE g.`name` NOT IN
+             (SELECT `name`
+                FROM `tv_genres` AS g
+	             INNER JOIN `tv_show_genres` AS s
+		     ON g.`id` = s.`genre_id`
+
+		     INNER JOIN `tv_shows` AS t
+		     ON s.`show_id` = t.`id`
+		     WHERE t.`title` = "Dexter")
+ ORDER BY g.`name`;

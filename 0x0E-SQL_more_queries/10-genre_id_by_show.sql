e0a905b0d1196fede2b7e9cb02ca111ab22fd421
Mostafa Mahmoud Lotfy <mlotfy516@gmail.com>
2024-01-20 19:43:48 +0200

    [FIX] Task 10 3

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,18 +1,10 @@
--- script that lists all shows contained in hbtn_0d_tvshows
-SELECT DISTINCT `name`
-  FROM `tv_genres` AS g
-       INNER JOIN `tv_show_genres` AS s
-       ON g.`id` = s.`genre_id`
+-- List all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 
-       INNER JOIN `tv_shows` AS t
-       ON s.`show_id` = t.`id`
-       WHERE g.`name` NOT IN
-             (SELECT `name`
-                FROM `tv_genres` AS g
-	             INNER JOIN `tv_show_genres` AS s
-		     ON g.`id` = s.`genre_id`
-
-		     INNER JOIN `tv_shows` AS t
-		     ON s.`show_id` = t.`id`
-		     WHERE t.`title` = "Dexter")
- ORDER BY g.`name`;
+SELECT DISTINCT t.`title`, s.`genre_id`
+FROM `tv_shows` AS t
+INNER JOIN `tv_show_genres` AS s ON t.`id` = s.`show_id`
+WHERE t.`id` IN (SELECT DISTINCT `show_id`
+                 FROM `tv_show_genres`)
+ORDER BY t.`title`, s.`genre_id`;
